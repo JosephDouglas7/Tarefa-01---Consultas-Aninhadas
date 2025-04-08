5ee6156956b784f72f30b7ca159f35ca2732bab1
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2025-04-08 15:00:45 -0300

    Update tarefa01-create.sql

--- tarefa01-create.sql
+++ tarefa01-create.sql
@@ -1,1 +1,9 @@
-
+ Select nome 
+      From funcionarios  
+      Where salario > (Select cod_gerente 
+             From funcionario 
+             Where codigo = (Select cod_gerente  
+             From departamento 
+             Where codigo = 2
+)
+); 
